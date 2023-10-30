b98f752ac1fd3c1d16a52a3870319ac86d4f6f24
ext.gaomengmeng6 <ext.gaomengmeng6@jd.com>
2023-10-30 19:03:13 +0800

    tbale function

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,6 +1,6 @@
 
-import React from "react";
 import { useEffect, useState } from 'react';
+import React from 'react';
 import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 
@@ -18,33 +18,35 @@ interface Indicator {
   showStyleLength: string | null;
   weight: string | null;
   children?: Indicator[];
-  parentObj?: Indicator
-  twoItem?: Indicator
-  oneItem?: Indicator
-  onCell: any
+  parents?: any
 }
 interface Iprops {
   data: Indicator[]
+  tableSpanNumber: number
+  filterDataConfig: (dataConfig: any) => void
+  columns: ColumnsType<Indicator>
 }
 interface Config {
-  (itm: Indicator, idx: number, data: Indicator[]): boolean,
+  (itm: Indicator, idx: number, data: Indicator[], spanIndex: number): boolean,
 }
 interface FunctionWithNumberArray {
-  (rulesFun: (itm: Indicator, idx: number, data: Indicator[])=> boolean): number[];
+  (rulesFun: (itm: Indicator, idx: number, data: Indicator[], spanIndex: number) => boolean, spanIndex: number): number[];
 }
 
 function table(props: Iprops) {
+  const [dataConfig, setDataConfig] = useState<any>(null)
 
-  const spansFilter: FunctionWithNumberArray = (rulesFun: Config) => {
+  const spansFilter: FunctionWithNumberArray = (rulesFun: Config, spanIndex: number) => {
     let pos = 0
     const spanArr: number[] = []
-    const data = props.data || []
+    const data = findLastLevelWithParents(props.data) || []
     data.forEach((itm, idx) => {
       if (idx === 0) {
         spanArr.push(1)
         return
       }
-      if (rulesFun(itm, idx, data)) {
+
+      if (rulesFun(itm, idx, data, spanIndex)) {
         spanArr[pos] = spanArr[pos] + 1
         spanArr.push(0)
         return
@@ -54,60 +56,52 @@ function table(props: Iprops) {
     })
     return spanArr
   }
-  const dataConfig = (() => {
-    const oneRule = (itm: Indicator, idx: number, data: Indicator[]) => {
-      return (itm?.twoItem as any).parentId === (data[idx - 1] as any)?.oneItem.indicatorId
-    }
-    const towRule = (itm: Indicator, idx: number, data: Indicator[]) => {
-      return itm.parentId === (data[idx - 1] as any)?.twoItem.indicatorId
-    }
-    return {
-      onespan: spansFilter(oneRule),
-      towspan: spansFilter(towRule)
-    }
-  })()
-  const columns: ColumnsType<Indicator> = [
-    {
-      title: '一级指标',
-      dataIndex: 'oneItem',
-      render: (text, record: any) => (
-        <span>
-          {record.oneItem.indicatorName}
-        </span>
-      ),
-      onCell: (_, index: any) => {
-        return {
-          rowSpan: dataConfig.onespan[index]
-        }
-      }
-    },
-    {
-      title: '二级指标',
-      dataIndex: 'oneItem',
-      render: (text, record: any) => (
-        <span>
-          {record.twoItem.indicatorName}
-        </span>
-      ),
-      onCell: (_, index: any) => {
-        return {
-          rowSpan: dataConfig.towspan[index]
-        }
-      }
-    },
-    {
-      title: '三级指标',
-      dataIndex: 'indicatorName',
-    },
-    {
-      title: '权重',
-      dataIndex: 'weight',
-    },
-  ];
+  function findLastLevelWithParents(data: Indicator[]) {
+    let result: Indicator[] = [];
 
-  return (
-    <Table pagination={false} rowKey={(record) => record.indicatorId} columns={columns} dataSource={props.data} bordered />
-  );
+    function findLastLevel(node: Indicator, parents: Indicator[]) {
+      if (node.children && node.children.length > 0) {
+        node.children.forEach((child) => {
+          findLastLevel(child, parents.concat(node));
+        });
+      } else {
+        result.push({ ...node, parents });
+      }
+    }
+
+    data.forEach((item) => {
+      findLastLevel(item, []);
+    });
+
+    return result;
+  }
+  const filterdataConfig = () => {
+    const rule = (itm: any, idx: number, data: Indicator[], spanIndex: number) => {
+
+      const parents = itm.parents
+      const sParents = data[idx - 1].parents
+      if (spanIndex < props.tableSpanNumber) {
+        return (parents[spanIndex] as any)?.parentId === sParents[spanIndex - 1]?.indicatorId
+      }
+      return itm.parentId === sParents[sParents.length - 1]?.indicatorId
+    }
+
+    const spansObj = {}
+    for (let index = 1; index <= props.tableSpanNumber; index++) {
+      const spanIndex = index
+      Object.assign(spansObj, { [`span${spanIndex}`]: spansFilter(rule, spanIndex) })
+    }
+    setDataConfig(spansObj)
+    props.filterDataConfig(spansObj)
+  }
+  useEffect(() => {
+    filterdataConfig()
+  }, [])
+
+
+  return <div>
+    {dataConfig && <Table pagination={false} rowKey={(record) => record.indicatorId} columns={props.columns} dataSource={findLastLevelWithParents(props.data)} bordered />}
+  </div>
 }
 
 export default table;
--- src/pages/Tetris/index.tsx
+++ src/pages/Tetris/index.tsx
@@ -1,7 +1,8 @@
 
-import React from "react";
+import React, { useState } from "react";
 import dataJson from './data.json'
 import DataTable from "@/components/DataTable";
+import type { ColumnsType } from 'antd/es/table';
 
 interface Indicator {
     indicatorId: string;
@@ -22,23 +23,55 @@ interface Indicator {
 }
 
 function App() {
-    const filterData = (data: Indicator[]): any => {
-        return data.map((oneItem: Indicator) => {
-            return oneItem.children?.map((twoItem: Indicator) => {
-                return twoItem.children?.map((thrreItem: Indicator) => {
-                    if (!thrreItem.children?.length) delete thrreItem.children
-                    return {
-                        ...thrreItem,
-                        twoItem,
-                        oneItem,
-                    }
-                }).flat()
-            }).flat()
-        }).flat()
+    const [dataConfig, setDataConfig] = useState<any>(null)
+    const filterData = (dataConfig: any) => {
+        setDataConfig(dataConfig)
     }
+    const columns: ColumnsType<Indicator> = [
+        {
+            title: '一级指标',
+            dataIndex: 'oneItem',
+            render: (text, record: any) => {
+                const [parent1Item] = record.parents
+                return <span>
+                    {parent1Item?.indicatorName}
+                </span>
+            },
+            onCell: (_, index: any) => {
+                return {
+                    rowSpan: dataConfig.span1[index]
+                }
+            }
+        },
+        {
+            title: '二级指标',
+            dataIndex: 'oneItem',
+            render: (text, record: any) => {
+                const [_, parent2Item] = record.parents
+                return <span>
+                    {parent2Item?.indicatorName}
+                </span>
+            },
+            onCell: (_, index: any) => {
+                return {
+                    rowSpan: dataConfig.span2[index]
+                }
+            }
+        },
+        {
+            title: '三级指标',
+            dataIndex: 'indicatorName',
+        },
+        {
+            title: '权重',
+            dataIndex: 'weight',
+        },
+    ];
+
     return (
         <div title="合并行">
-            <DataTable data={filterData(dataJson.data)} />
+
+            <DataTable columns={columns} data={dataJson.data} tableSpanNumber={2} filterDataConfig={filterData} />
         </div>
     );
 }
