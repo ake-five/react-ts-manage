ab5464322f43b8f493073ade098743f273553894
ext.gaomengmeng6 <ext.gaomengmeng6@jd.com>
2023-12-12 17:14:37 +0800

    del: del mian qiankun 配置

--- src/main.tsx
+++ src/main.tsx
@@ -7,24 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 
-
-
-// import { registerMicroApps, start } from 'qiankun';
-
-// const apps = [
-//     {
-//         name: '子应用1',
-//         entry: 'http://localhost:8080', // 子应用1的入口地址
-//         container: '#subapp-container', // 子应用1渲染的容器
-//         activeRule: '/app1' // 匹配的路径规则
-//     },
-//     // 添加其他子应用的配置...
-// ];
-
-// registerMicroApps(apps);111
-
-// start();
-
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <Provider store={store}>
